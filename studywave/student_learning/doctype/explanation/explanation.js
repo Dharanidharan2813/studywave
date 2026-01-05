937e5cb6d82777e3a47a2f734f8d163fcaa7a687
sarathibalamurugan <sarathigreen4@gmail.com>
2026-01-05 15:27:19 +0530

    feat: add explanation field in quiz and user log

--- studywave/student_learning/doctype/explanation/explanation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, dharanidharan s and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Explanation", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- studywave/student_learning/doctype/lesson/lesson.py
+++ studywave/student_learning/doctype/lesson/lesson.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
@@ -11,34 +12,28 @@ class Lesson(Document):
 
 @frappe.whitelist()
 def check_option(ans, lesson):
-	pass
-	# exist = frappe.db.exists("User Log", {"student": frappe.session.user, "lesson": lesson})
-	# if not exist:
-	#     frappe.msgprint("Called")
-	#     frappe.msgprint(ans)
-	#     frappe.msgprint(lesson)
-
-	#     ans = frappe.parse_json(ans)
-	#     quizzes = frappe.get_doc('Lesson',lesson)
-	#     log = frappe.new_doc('User Log')
-	#     log.student = frappe.session.user
-	#     log.lesson = lesson
-	#     for q,answer in zip(quizzes.question, ans):
-	#          new_quiz = log.append('quiz', {})
-	#          new_quiz.question = q.question
-	#          new_quiz.correct_answer = q.correct_answer
-	#          new_quiz.answer = answer['value']
-	#          new_quiz.topic = q.topic
-	#          new_quiz.correct_explanation = q.correct_explanation
-
-	#     log.insert(ignore_permissions=True)
-
-	#     frappe.msgprint("Called")
-
-	#     for quiz,answer in zip(quizzes.question, ans):
-	#         if quiz.correct_answer == answer['value']:
-	#             frappe.msgprint("Correct Answer")
-	#         else:
-	#             frappe.msgprint("Wrong Answer")
-	# else:
-	#     frappe.msgprint("You have already attempted this lesson.")
+	# pass
+	exist = frappe.db.exists("User Log", {"student": frappe.session.user, "lesson": lesson})
+	if not exist:
+		ans = frappe.parse_json(ans)
+		quizzes = frappe.get_doc("Lesson", lesson)
+		log = frappe.new_doc("User Log")
+		log.student = frappe.session.user
+		log.lesson = lesson
+		log.total_marks = len(quizzes.question)
+		marks = 0
+		for q, answer in zip(quizzes.question, ans, strict=True):
+			new_quiz = log.append("quiz", {})
+			new_quiz.question = q.question
+			new_quiz.correct_answer = q.correct_answer
+			new_quiz.answer = answer["value"]
+			new_quiz.topic = q.topic
+			new_quiz.explanation = answer["explanation"]
+			new_quiz.correct_explanation = q.correct_explanation
+			if new_quiz.correct_answer == new_quiz.answer:
+				marks += 1
+		log.obtained_marks = marks
+		log.insert(ignore_permissions=True)
+		frappe.msgprint(_("Submitted successfully."))
+	else:
+		frappe.msgprint(_("You have already attempted this lesson."))
